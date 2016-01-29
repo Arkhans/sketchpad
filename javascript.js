a18bc0439b736084f79d3257fd70a767f108071a
Greg Kavvadias <gregkavvadias@gmail.com>
2016-01-29 12:25:30 +0200

    Have a working grid generator

--- javascript.js
+++ javascript.js
@@ -1,12 +1,39 @@
 $(document).ready(function() {
-  for (i = 0; i < 4; i++) {
-    var column = $("<div class='column'></div>");
-    for (j = 0; j < 4; j++) {
-      var unit = $("<div class='unit'></div>");
-      $('.unit').css("width", (960 / 4));
-      $('.unit').css("height", (960 / 4));
-      column.append(unit);
+  createGrid(16);
+
+  $("#draw").on("click", function() {
+    removeGrid();
+    createGrid();
+  });
+
+  $("#txt").keypress(function(e) {
+    if (e.which == 13) {
+    removeGrid();
+    createGrid();
+    return false;
     }
-    $("#container").append(column);
-  }
+  });
 });
+
+function createGrid() {
+  var dim = $("input[name='grid']").val();
+  if (dim > 100) {
+    $(".error").show();
+    return false;
+  }
+  for (i = 0; i < dim; i++) {
+      var column = $("<div class='column'></div>");
+      for (j = 0; j < dim; j++) {
+        var unit = $("<div class='unit'></div>");
+        unit.css("width", (960 / dim));
+        unit.css("height", (960 / dim));
+        column.append(unit);
+      }
+      $("#container").append(column);
+  }
+} 
+
+function removeGrid() {
+  $("#container").empty();
+}
+
